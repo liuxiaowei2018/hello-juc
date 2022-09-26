93c46fad431443036143da0acb2896f9fc92a718
刘校伟 <liuxiaowei@pharmakeyring.com>
2022-09-26 13:05:28 +0800

    style: 并发编程

---
+++ src/main/java/com/open/concurrent/HelloConcurrentApplication.java
@@ -0,0 +1,16 @@
+package com.open.concurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liuxiaowei
+ * @date 2022年09月26日 10:37
+ * @Description
+ */
+@SpringBootApplication
+public class HelloConcurrentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloConcurrentApplication.class, args);
+    }
+}
