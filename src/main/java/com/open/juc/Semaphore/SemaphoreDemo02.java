2f105e948018ded3a9ee57066c05d88d7755e681
刘校伟 <liuxiaowei@pharmakeyring.com>
2022-11-15 10:49:02 +0800

    style: Semaphore 信号量测试

---
+++ src/main/java/com/open/juc/Semaphore/SemaphoreDemo02.java
@@ -0,0 +1,45 @@
+package com.open.juc.Semaphore;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author liuxiaowei
+ * @date 2022年09月26日 15:54
+ * @Description
+ */
+@Slf4j
+public class SemaphoreDemo02 {
+
+    private final static int threadCount = 20;
+
+    public static void main(String[] args) throws Exception {
+
+        ExecutorService exec = Executors.newCachedThreadPool();
+
+        final Semaphore semaphore = new Semaphore(3);
+
+        for (int i = 0; i < threadCount; i++) {
+            final int threadNum = i;
+            exec.execute(() -> {
+                try {
+                    semaphore.acquire(3); // 获取多个许可
+                    test(threadNum);
+                    semaphore.release(3); // 释放多个许可
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+            });
+        }
+        exec.shutdown();
+    }
+
+    private static void test(int threadNum) throws Exception {
+        log.info("{}", threadNum);
+        Thread.sleep(1000);
+    }
+}
